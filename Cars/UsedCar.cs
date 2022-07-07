4cfd26a4584b22811f5a3a096aa1fec1f861d713
Travis <travisamador@gmail.com>
2022-07-07 15:22:11 -0400

    Added UsedCar class

--- Cars/UsedCar.cs
+++ Cars/UsedCar.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Cars
 {
-    internal class UsedCar
+    internal class UsedCar : Car
     {
+        //properties
+        double Mileage {get; set;}
+        //Constructor
+        public UsedCar(string _make, string _model, int _year, decimal _price, double _mileage) : base(_make, _model, _year, _price)
+        {
+            Mileage = _mileage;
+        }
+        //methods
+        public override string ToString()
+        {
+            return base.ToString() + $"\t{Mileage}";
+        }
     }
 }
